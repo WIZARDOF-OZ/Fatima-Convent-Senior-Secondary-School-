d22e1eca8c74db3fbfb64ca65b5793bf68de42de
Wizard <you@example.com>
2022-11-19 23:56:41 +0530

    Added New Files And Some Content

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
+//Hide Loading Box (Preloader)
+function handlePreloader() {
+    if ($('.preloader').length) {
+        $('body').addClass('page-loaded');
+        $('.preloader').delay(800).fadeOut(300);
+    }
+}
+
+
+
+
+
+
 AOS.init();
