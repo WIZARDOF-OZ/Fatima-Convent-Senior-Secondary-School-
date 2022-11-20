33dc94f04e92c35fcb11636dfbbea1180d8463e9
Wizard <you@example.com>
2022-11-20 14:33:09 +0530

    Added Animation Using Animate Library

--- js/main.js
+++ js/main.js
@@ -1,14 +1,2 @@
-//Hide Loading Box (Preloader)
-function handlePreloader() {
-    if ($('.preloader').length) {
-        $('body').addClass('page-loaded');
-        $('.preloader').delay(800).fadeOut(300);
-    }
-}
+// AOS.init();
 
-
-
-
-
-
-AOS.init();
