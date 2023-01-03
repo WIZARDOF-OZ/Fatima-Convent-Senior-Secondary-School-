4bdbebadd8cc7ae8dd1de9da4c35cd4242751c91
WIZARD OF OZ <nurajabegum81@gmail.com>
2023-01-03 23:35:44 +0530

    Made A Big Changes

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,19 +1,14 @@
-	// Login Page Code Goes Here
-    function validate()
-    {
-    var username=document.getElementById("username").value;
-    var password=document.getElementById("password").value;
-    if(username=="admin"&& password=="user")
-    {
-        //  alert("login succesfully");
-       window.open('./admin/index.html')
-        return false;
-    
-    }
-    else
-    {
-        window.open('error-404.html')
-    }
-    
-    
-    }
+// Login Page Code Goes Here
+function validate()
+
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+if (username == "admin" && password == "user") {
+    //  alert("login succesfully");
+    window.open('./admin/index.html')
+    return false;
+
+}
+else {
+    alert("Login Failed")
+}
